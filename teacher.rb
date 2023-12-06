84c7e1aa7411aa6cbe96ac344b731347fbaae22a
George <georgie.george7@gmail.com>
2023-12-06 17:41:03 +0400

    Fix Teacher class initialization

--- teacher.rb
+++ teacher.rb
@@ -3,8 +3,8 @@ require_relative 'person'
 class Teacher < Person
   attr_reader :specialization
 
-  def initialize(id, specialization, name: 'Unknown', age: nil, parent_permission: true)
-    super(id, name: name, age: age, parent_permission: parent_permission)
+  def initialize(id, specialization, name = 'Unknown', age: nil, parent_permission: true)
+    super(id, name, age: age, parent_permission: parent_permission)
     @specialization = specialization
   end
 
